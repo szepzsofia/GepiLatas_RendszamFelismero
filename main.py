8eaef7a23ada010e779483c7feac50852e2ee78d
szepzsofia <113353852+szepzsofia@users.noreply.github.com>
2022-11-28 13:30:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,53 +5,53 @@ import messagebox
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract'
 
-image = cv2.imread('test4.jpg')
-image = imutils.resize(image, width=300)
-cv2.imshow("Alap kep", image)
+kep = cv2.imread('test.jpg')
+kep = imutils.resize(kep, width=300) #Kép átméretezése
+cv2.imshow("Alap kep", kep) #Megjelenik az alap kép átméretezve
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Szurke arnyalatos kep", gray_image)
+szurke_kep = cv2.cvtColor(kep, cv2.COLOR_BGR2GRAY) #Kép színének szürkeárnyalatossá tétele
+cv2.imshow("Szurke arnyalatos kep", szurke_kep) #Szürkeárnyalatos kép megjelenítése
 
-gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
-cv2.imshow("Simitott kep", gray_image)
+szurke_kep = cv2.bilateralFilter(szurke_kep, 11, 17, 17) #Kép simítása
+cv2.imshow("Simitott kep", szurke_kep) #Simított kép megjelenítése
 
-edged = cv2.Canny(gray_image, 30, 200)
-cv2.imshow("El detektalas", edged)
+szel = cv2.Canny(szurke_kep, 30, 200) #Kép éleinek detektálása
+cv2.imshow("El detektalas", szel)
 
-cnts,new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-image1=image.copy()
-cv2.drawContours(image1,cnts,-1,(0,255,0),3)
-cv2.imshow("Konturok",image1)
+kontur,new = cv2.findContours(szel.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #Összes kontur keresése
+kepmas=kep.copy()
+cv2.drawContours(kepmas,kontur,-1,(0,255,0),3) #Konturok kirajzolása
+cv2.imshow("Konturok",kepmas)
 
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True) [:10]
+kontur = sorted(kontur, key = cv2.contourArea, reverse = True) [:10] #Konturok kikeresése, legjobb 10 kiválasztása
 screenCnt = None
-image2 = image.copy()
-cv2.drawContours(image2,cnts,-1,(0,255,0),3)
-cv2.imshow("Top 10 kontur",image2)
+kepmas2 = kep.copy()
+cv2.drawContours(kepmas2,kontur,-1,(0,255,0),3)
+cv2.imshow("Top 10 kontur",kepmas2)
+
+
+kivagott = './7.png'
+cv2.imshow("Korbevagott", cv2.imread(kivagott))
 
 i=7
-for c in cnts:
+for c in kontur:
         perimeter = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * perimeter, True)
         if len(approx) == 4:
                 screenCnt = approx
 
         x, y, w, h = cv2.boundingRect(c)
-        new_img = image[y:y + h, x:x + w]
+        new_img = kep[y:y + h, x:x + w]
         cv2.imwrite('./' + str(i) + '.png', new_img)
         i += 1
         break
 
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
-cv2.imshow("Rendszam tabla", image)
+cv2.drawContours(kep, [screenCnt], -1, (0, 255, 0), 3)
+cv2.imshow("Rendszam tabla", kep)
 
-kivagott = './7.png'
-cv2.imshow("Korbevagott", cv2.imread(kivagott))
 
-rendsz = pytesseract.image_to_string(kivagott, lang='eng')
+rendszam = pytesseract.image_to_string(kivagott, lang='eng')
 
-messagebox.showinfo(title="Erzelekt Rendszam", message=rendsz)
+messagebox.showinfo(title="Erzekelt Rendszam", message=rendszam)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
